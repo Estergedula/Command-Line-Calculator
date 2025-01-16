f831222be4624595288f8d23ff52e9283bb7a93f
= <=>
2025-01-16 15:58:14 +0200

    start calculate expression function

--- Calculator/Calculator.h
+++ Calculator/Calculator.h
@@ -1,19 +1,13 @@
 #pragma once
+#include <string>
 
 class Calculator
 {
 public:
 	Calculator();
 	~Calculator();
+	int expressionCalculate(string);
 
 private:
 
 };
-
-Calculator::Calculator()
-{
-}
-
-Calculator::~Calculator()
-{
-}
