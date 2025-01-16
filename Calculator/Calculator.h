9e2a45a8491ab4a6608fafb19c9ca96697b081c8
Kibelevich <m0556780320@gmail.com>
2025-01-16 14:37:17 +0200

    continue the main

--- Calculator/Calculator.h
+++ Calculator/Calculator.h
@@ -1,1 +1,19 @@
 #pragma once
+
+class Calculator
+{
+public:
+	Calculator();
+	~Calculator();
+
+private:
+
+};
+
+Calculator::Calculator()
+{
+}
+
+Calculator::~Calculator()
+{
+}
--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -4,19 +4,39 @@ using namespace std;
 
 int main()
 {
-	cout<< "======================\n" << " Command-Line Calculator\n" << "======================\n\n";
+	cout << "======================\n"
+		<< " Command-Line Calculator\n"
+		<< "======================\n\n";
 	string expression;
+	string equation;
+	int start, end;
 	int choice;
 	do
 	{
-		cout << "Type 1 to calculate an expression.\n 2 for finding prime numbers.\n"
-			<< "3 for solving equations.\n 0 to exit.\n";
+		cout << "Type 1 to calculate an expression.\n"
+			<< "2 for finding prime numbers.\n"
+			<< "3 for solving equations (first or second order).\n"
+			<< "0 to exit.\n";
 		cin >> choice;
 		switch (choice)
 		{
 		case 1:
 			cout << "Enter an expression" << endl;
-			cin.getline(expression);
+			getline(cin, expression);
+			// 
+			break;
+		case 2:
+			cout << "Enter a range of numbers." << endl;
+			cin >> start >> end;
+			// 
+			break;
+		case 3:
+			cout << "Enter an equation." << endl;
+			getline(cin, equation);
+			// 
+			break;
+		case 0:
+			break;
 		default:
 			break;
 		}
